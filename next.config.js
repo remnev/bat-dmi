30ed3363199b4e5281510f5ff34842b49aedf87b
Yuriy Remnev <yura_remnev@mail.ru>
2019-01-16 23:14:10 +0300

    initial

---
+++ next.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  exportPathMap: function () {
+    return {
+      '/': { page: '/' }
+    }
+  }
+};
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Layout from '../components/layout.js'
+
+export default () => (
+  <Layout>
+    <h1>Title</h1>
+  </Layout>
+);
